21b6f0e5c3f27256f17381d3aca14860aec3daa4
w3cways <83630950@qq.com>
2022-06-22 10:30:32 +0800

    other:添加窗口，以便复现问题

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
-const { app, globalShortcut } = require('electron')
-const Screenshots = require('electron-screenshots').default
+const { app, globalShortcut, BrowserWindow } = require("electron");
+const Screenshots = require("electron-screenshots").default;
 
 app.whenReady().then(() => {
-  const screenshots = new Screenshots()
-  
-  globalShortcut.register('ctrl+shift+a', () => {
-    screenshots.startCapture()
-  })
+  const screenshots = new Screenshots();
+
+  globalShortcut.register("ctrl+shift+a", () => {
+    screenshots.startCapture();
+  });
+
+  const mainWin = new BrowserWindow({
+    width: 600,
+    height: 600,
+    frame: false,
+    titleBarStyle: "hidden",
+    trafficLightPosition: { x: 5, y: 24 },
+    title: "Henglink",
+    titleBarOverlay: true,
+    show: true,
+  });
+  mainWin.removeMenu();
+  mainWin.loadURL("https://www.baidu.com");
 
   // 防止不能关闭截图界面
-  globalShortcut.register('ctrl+shift+q', () => {
-    app.quit()
-  })
-})
+  globalShortcut.register("ctrl+shift+q", () => {
+    app.quit();
+  });
+});
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
